664b2a7852a470dfc802d39edc6d0301cd098b41
POI99 <poi991024@gmail.com>
2024-07-25 16:46:43 +0900

    [김수찬] - Entity 수정 완료

--- src/main/java/com/green/glampick/entity/UserEntity.java
+++ src/main/java/com/green/glampick/entity/UserEntity.java
@@ -11,46 +11,50 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Slf4j
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "user")
-public class UserEntity {
+// 유저 테이블
+public class UserEntity extends UpdatedAt {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long userId;  // 유저 PK
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Comment("유저 ID")
+    private Long userId;
 
-    private String userEmail;  // 유저 이메일
+    @Column(nullable = false, unique = true) @Comment("유저 이메일")
+    private String userEmail;
 
-    @Setter
-    private String userPw;  // 유저 비밀번호
+    @Column(nullable = false) @Comment("쿠폰 ID")
+    private String userPw;
 
-    private String userName;  // 유저 실명
+    @Column(nullable = false) @Comment("유저 실명")
+    private String userName;
 
-    @Setter
-    private String userNickname;  // 유저 닉네임
+    @Column(nullable = false, unique = true) @Comment("유저 닉네임")
+    private String userNickname;
 
-    @Setter
-    private String userPhone;  // 유저 휴대폰 번호
+    @Comment("유저 휴대폰 번호")
+    private String userPhone;
 
-    @Setter
-    private String userProfileImage;  // 유저 프로필 이미지
+    @Comment("유저 프로필 이미지")
+    private String userProfileImage;
 
-    private String userRole;  // 유저 권한
+    @Column(nullable = false) @Enumerated(value = EnumType.STRING) @Comment("유저 권한")
+    private Role userRole;
 
-    private String providerId;  // 소셜 유저 ID
+    @Comment("소셜 유저 ID")
+    private String providerId;
 
-    private String userSocialType;  // 소셜 로그인 타입
-
-    @CreationTimestamp
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    @Column(nullable = false) @Enumerated(value = EnumType.STRING) @Comment("소셜 로그인 타입")
+    private SignInProviderType userSocialType;
 
     public UserEntity(SignUpRequestDto dto) {
         this.userEmail = dto.getUserEmail();
