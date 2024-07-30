9afb69da51af2f58fbdb8c9f348e686f244d66f2
Administrator <redraib05@gmail.com>
2024-07-30 18:04:16 +0900

    [3차] GangGuk 오너답글작성 작업[ING]

--- src/main/java/com/green/glampick/repository/OwnerRepository.java
+++ src/main/java/com/green/glampick/repository/OwnerRepository.java
@@ -1,7 +1,8 @@
 package com.green.glampick.repository;
 
+import com.green.glampick.entity.OwnerEntity;
 import com.green.glampick.entity.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OwnerRepository extends JpaRepository<ReviewEntity, Long> {
+public interface OwnerRepository extends JpaRepository<OwnerEntity, Long> {
 }
