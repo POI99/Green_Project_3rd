9a2767475c7febf06c248ad051a87655b83b1f5b
Administrator <redraib05@gmail.com>
2024-08-01 18:15:22 +0900

    [3차] GangGuk 오너리뷰불러오기 [FINISH]

--- src/main/java/com/green/glampick/dto/request/user/GetReviewRequestDto.java
+++ src/main/java/com/green/glampick/dto/request/user/GetReviewRequestDto.java
@@ -2,14 +2,19 @@ package com.green.glampick.dto.request.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.green.glampick.common.Paging;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import static com.green.glampick.common.GlobalConst.PAGING_SIZE;
 
 @Setter
 @Getter
+@ToString
 public class GetReviewRequestDto extends Paging {
+    @Schema(example = "0||1")
+    private long typeNum;
 
     @JsonIgnore private Long reviewId;
     @JsonIgnore private long userId;
