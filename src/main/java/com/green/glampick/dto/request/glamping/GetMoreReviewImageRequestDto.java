81cc53cccc12eef06ae7b89196c72f661a9ef052
Administrator <mj17428@naver.com>
2024-08-02 12:18:22 +0900

    [김민지] - 성공 response 수정

--- src/main/java/com/green/glampick/dto/request/glamping/GetMoreReviewImageRequestDto.java
+++ src/main/java/com/green/glampick/dto/request/glamping/GetMoreReviewImageRequestDto.java
@@ -2,13 +2,9 @@ package com.green.glampick.dto.request.glamping;
 
 import com.green.glampick.common.GlobalConst;
 import com.green.glampick.common.Paging;
-import com.green.glampick.common.response.ResponseCode;
-import com.green.glampick.common.response.ResponseMessage;
-import com.green.glampick.dto.object.ReviewListItem;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
