829baaf0b3d481d50fd8b1db845761f3b5aca9f4
POI99 <poi991024@gmail.com>
2024-08-12 19:15:24 +0900

    [김수찬] - 구현 최종 완료

--- src/main/java/com/green/glampick/dto/response/admin/GetAccessGlampingInfoResponseDto.java
+++ src/main/java/com/green/glampick/dto/response/admin/GetAccessGlampingInfoResponseDto.java
@@ -3,19 +3,43 @@ package com.green.glampick.dto.response.admin;
 import com.green.glampick.dto.ResponseDto;
 import com.green.glampick.entity.GlampingEntity;
 import com.green.glampick.entity.OwnerEntity;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static com.green.glampick.common.GlobalConst.SUCCESS_CODE;
 import static com.green.glampick.common.GlobalConst.SUCCESS_MESSAGE;
 
+@Getter
+@Setter
 public class GetAccessGlampingInfoResponseDto extends ResponseDto {
 
-    private GlampingEntity glampingEntity;
+    private Long ownerId;
+    private String glampName;
+    private String glampCall;
+    private String glampImage;
+    private String glampLocation;
+    private String region;
+    private Integer extraCharge;
+    private String glampIntro;
+    private String infoBasic;
+    private String infoNotice;
+    private String traffic;
 
     private GetAccessGlampingInfoResponseDto(GlampingEntity glampingEntity) {
         super(SUCCESS_CODE, SUCCESS_MESSAGE);
-        this.glampingEntity = glampingEntity;
+        this.ownerId = glampingEntity.getOwner().getOwnerId();
+        this.glampName = glampingEntity.getGlampName();
+        this.glampCall = glampingEntity.getGlampCall();
+        this.glampImage = glampingEntity.getGlampImage();
+        this.glampLocation = glampingEntity.getGlampLocation();
+        this.region = glampingEntity.getRegion();
+        this.extraCharge = glampingEntity.getExtraCharge();
+        this.glampIntro = glampingEntity.getGlampIntro();
+        this.infoBasic = glampingEntity.getInfoBasic();
+        this.infoNotice = glampingEntity.getInfoNotice();
+        this.traffic = glampingEntity.getTraffic();
     }
 
     public static ResponseEntity<GetAccessGlampingInfoResponseDto> success(GlampingEntity glampingEntity) {
